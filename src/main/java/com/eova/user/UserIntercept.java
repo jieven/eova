c79d2873af054e2457dfb2d810c22d4ee4533c1c
Jieven <admin@eova.cn>
2018-09-02 13:52:59 +0800

    新增帐号唯一校验

--- src/main/java/com/eova/user/UserIntercept.java
+++ src/main/java/com/eova/user/UserIntercept.java
@@ -8,7 +8,10 @@ package com.eova.user;
 
 import com.eova.aop.AopContext;
 import com.eova.aop.MetaObjectIntercept;
+import com.eova.common.Easy;
 import com.eova.common.utils.EncryptUtil;
+import com.eova.common.utils.xx;
+import com.jfinal.plugin.activerecord.Db;
 
 /**
  * 自定义用户管理拦截器
@@ -19,9 +22,17 @@ public class UserIntercept extends MetaObjectIntercept {
 
 	@Override
 	public String addBefore(AopContext ac) throws Exception {
+		// 数据服务端校验
+		String loginId = ac.record.getStr("login_id");
+		Long num = Db.use(xx.DS_EOVA).queryLong("select count(*) from eova_user where login_id = ?", loginId);
+		if (num > 0) {
+			return Easy.warn("帐号重复,请重新填写!");
+		}
+
 		// 新增时密码加密储存
 		String str = ac.record.getStr("login_pwd");
 		ac.record.set("login_pwd", EncryptUtil.getSM32(str));
+
 		return null;
 	}
 
